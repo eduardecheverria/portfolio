10c032498faee71ec668358e7856fbeccdb0cdf0
Eduardo Echeverria Martin del campo <eduardo@Mac15.local>
2023-11-27 17:59:37 -0600

    animation keyboard and turning head

--- src/pages/components/ProgrammerBody/ProgrammerBody.tsx
+++ src/pages/components/ProgrammerBody/ProgrammerBody.tsx
@@ -4,7 +4,18 @@ const ProgrammerBody = () => {
   return (
     <div className={styles.programmerBodyContainer}>
         <div className={styles.body}>
-            <div className={styles.bodyHead}></div>
+              <div className={styles.bodyHead}>
+                <div className={styles.eyebrow}>
+                      
+                </div>
+                <div className={styles.bodyHeadEye}>
+                      <div className={styles.bodyHeadEyeIris}>
+                          
+                      </div>
+                </div>
+                <div className={styles.bodyHeadNose}></div>
+                <div className={styles.bodyHeadMouth}></div>  
+            </div>
             <div className={styles.bodyNeck}></div>
               <div className={styles.bodyBody}>
                 <div className={styles.bodyArmOutOfPlaceContainer}>
