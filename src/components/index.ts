5f1cc9cf3c92eb17c33482615c6fcb6daee698d7
Eduardo Echeverria Martin del campo <eduardo.echeverria.dev@outlook.com>
2026-04-21 10:45:00 -0600

    refactor: barrel exports for components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { Hud } from './Hud';
+export { Hero } from './Hero';
+export { Stats } from './Stats';
+export { Missions } from './Missions';
+export { Quests } from './Quests';
+export { Footer } from './Footer';
+export { CursorTrail } from './CursorTrail';
